c10cd7df40fa4dedf4b8a379cda5e7500cbd0677
Dennis Kao <dshnkao@gmail.com>
2017-03-04 21:35:27 +1100

    emacs - org dir

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -334,15 +334,19 @@ you should place your code here."
   (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
   (add-to-list 'load-path "~/.spacemacs.d")
 
+  (with-eval-after-load 'org
+    (setq org-agenda-files '("~/Sync/documents/org/")))
+
   (when (eq system-type 'darwin)
     (mac-auto-operator-composition-mode)
     (custom-set-variables '(markdown-command "/usr/local/bin/pandoc"))
     )
   (when (eq system-type 'gnu/linux)
-    (load "firacode.el"))
+    (load "firacode.el")
     (setq helm-dash-docset-newpath "~/.local/share/Zeal/Zeal/docsets")
     (custom-set-variables '(markdown-command "/usr/bin/pandoc"))
     )
+  )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.

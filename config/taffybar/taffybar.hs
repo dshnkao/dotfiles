a0bcb7184f7dcb0bb99470825da9d3908c36c275
Dennis Kao <dshnkao@gmail.com>
2017-08-28 01:21:08 +1000

    lazy

--- config/taffybar/taffybar.hs
+++ config/taffybar/taffybar.hs
@@ -5,6 +5,7 @@ import System.Taffybar
 import System.Taffybar.Battery
 import System.Taffybar.FreedesktopNotifications
 import System.Taffybar.MPRIS
+import System.Taffybar.NetMonitor
 import System.Taffybar.Pager
 import System.Taffybar.SimpleClock
 import System.Taffybar.Systray
@@ -12,30 +13,16 @@ import System.Taffybar.TaffyPager
 import System.Taffybar.Weather
 import System.Taffybar.Widgets.PollingBar
 import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.Widgets.PollingLabel
 import System.Taffybar.Widgets.VerticalBar
+import System.Process (readProcess)
 import Text.Printf (printf)
-memCallback = do
-  mi <- parseMeminfo
-  return [memoryUsedRatio mi]
 
-cpuCallback = do
-  (userLoad, systemLoad, totalLoad) <- cpuLoad
-  return [totalLoad, systemLoad]
-
+main :: IO ()
 main = do
-  let memCfg = defaultGraphConfig { graphDataColors = [(1, 0, 0, 1)]
-                                  , graphLabel = Nothing
-                                  }
-      cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1)
-                                                      , (1, 0, 1, 0.5)
-                                                      ]
-                                  , graphLabel = Nothing
-                                  }
-      mem   = pollingGraphNew memCfg 1 memCallback
-      cpu   = pollingGraphNew cpuCfg 0.5 cpuCallback
   defaultTaffybar defaultTaffybarConfig
     { startWidgets = [ pager ]
-    , endWidgets   = [ clock, textBattery, mem, cpu ]
+    , endWidgets   = [ clock, textBattery, mem, cpu, network, wifi ]
     }
 
 pager :: IO Widget
@@ -53,6 +40,44 @@ pager = taffyPagerNew PagerConfig
     pad = wrap " " " "
     noScratchPad ws = if ws == "NSP" then "" else pad ws
     nodots ws = if ws == "..." then "" else ws
+
+wifi :: IO Widget
+wifi =
+  let
+    f = flip fmap (readProcess "nmcli" ["device"] "") $ \s ->
+      maybe (colorize "#aaaaaa" "" "\61931 No Connection") id $
+      case drop 1 (lines s) of
+        []   -> Nothing
+        x:xs -> case words x of
+          device:typ:state:connection:[] -> if state /= "connected"
+            then Nothing
+            else pure $ "\61931 " ++ connection
+          _ -> Nothing
+  in do
+    label <- pollingLabelNew "\61931 No Connection" 1 f
+    widgetShowAll label
+    return label
+
+network :: IO Widget
+network = netMonitorNewWith 1 "wlp4s0" 2 "▼ $inKB$kb/s ▲ $outKB$kb/s"
+
+cpu :: IO Widget
+cpu = pollingGraphNew defaultGraphConfig
+  { graphDataColors = [ (1, 0, 0, 1) ]
+  , graphLabel = Nothing
+  , graphDirection = RIGHT_TO_LEFT
+  } 1 callback
+  where
+    callback = (\(userLoad, sysLoad, totalLoad) -> [totalLoad]) <$> cpuLoad
+
+mem :: IO Widget
+mem = pollingGraphNew defaultGraphConfig
+  { graphDataColors = [ (1, 0, 0, 1) ]
+  , graphLabel = Nothing
+  , graphDirection = RIGHT_TO_LEFT
+  } 1 callback
+  where
+    callback = (:[]) . memoryUsedRatio <$> parseMeminfo
 
 battery :: IO Widget
 battery = batteryBarNew BarConfig

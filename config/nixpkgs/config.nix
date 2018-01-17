77bb15e8baa4489d5b88fa1f05be12a53a863853
Dennis Kao <dshnkao@gmail.com>
2018-01-17 13:06:50 +1100

    nix - add user packages

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -1,3 +1,103 @@
 {
   allowUnfree = true;
+  packageOverrides = pkgs: rec {
+
+    coreEnv = with pkgs; buildEnv {
+      name = "coreEnv";
+      paths = [
+        exa
+        fd
+        feh
+        fzf
+        nix-prefetch-git
+        nix-prefetch-scripts
+        nix-repl
+        pass
+        ranger
+        ripgrep
+        rofi
+        zathura
+      ];
+    };
+
+    baseEnv = with pkgs; buildEnv {
+      name = "baseEnv";
+      paths = [
+        ag
+        aspell
+        aspellDicts.en
+        cloc
+        evince
+        ffmpeg
+        file
+        htop
+        httpie
+        imagemagick
+        jq
+        ktorrent
+        libreoffice
+        neofetch
+        nmap
+        pandoc
+        pmount
+        scrot
+        skypeforlinux
+        smartmontools
+        sqlite
+        steam
+        steam-run
+        stow
+        stress
+        texlive.combined.scheme-full
+        tree
+        unrar
+        unzip
+        vlc
+        youtube-dl
+        zip
+      ];
+    };
+
+    coreDev = with pkgs; buildEnv {
+      name = "coreDev";
+      paths = [
+        binutils
+        cabal2nix
+        gcc
+        git
+        gnumake
+        haskellPackages.cabal-install
+        haskellPackages.ghc
+        haskellPackages.hindent
+        haskellPackages.hlint
+        haskellPackages.styx
+        haskellPackages.ghcid
+        jetbrains.idea-community
+        nodePackages.tern
+        openjdk8
+        patchelf
+        python
+        python36
+        sbt
+        scala
+        shellcheck
+        vscode
+      ];
+    };
+
+    baseDev = with pkgs; buildEnv {
+      name = "baseDev";
+      paths = [
+        cargo
+        clojure
+        go
+        gradle
+        kotlin
+        leiningen
+        maven
+        nodejs
+        rustc
+      ];
+    };
+  };
 }

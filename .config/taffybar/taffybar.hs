ccf642bcdfa707d8820d464372acbfa936d5b659
Dennis Kao <dshnkao@gmail.com>
2018-06-22 21:53:10 +1000

    taffybar - update to 2.x

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -1,31 +1,10 @@
-{-# LANGUAGE MultiWayIf #-}
---import Control.Monad (join)
---import Data.List (intercalate)
---import Data.Maybe (fromMaybe)
---import Data.Monoid ((<>))
---import Graphics.UI.Gtk
---import Graphics.UI.Gtk.Abstract.Widget
---import System.Taffybar.Information.CPU
---import System.Taffybar.Information.Memory
---import System.Taffybar
---import System.Taffybar.Battery
---import System.Taffybar.FreedesktopNotifications
---import System.Taffybar.MPRIS
---import System.Taffybar.NetMonitor
---import System.Taffybar.Pager
---import System.Taffybar.Widget.SimpleClock
---import System.Taffybar.Systray
---import System.Taffybar.TaffyPager
---import System.Taffybar.Widget.Generic.PollingBar
---import System.Taffybar.Widget.Generic.PollingGraph
---import System.Taffybar.Widget.Generic.PollingLabel
---import System.Taffybar.Widget.Generic.VerticalBar
---import System.Taffybar.Information.Battery
---import System.Process (readProcess)
---import Text.Printf (printf)
---import Control.Monad.Trans.Maybe (MaybeT(..))
-
+import Control.Monad (join)
+import Control.Monad.IO.Class (MonadIO(..))
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import System.Process (readCreateProcess, shell)
 import System.Taffybar
+import System.Taffybar.Context (TaffyIO)
 import System.Taffybar.Hooks
 import System.Taffybar.Information.CPU
 import System.Taffybar.Information.Memory
@@ -35,6 +14,9 @@ import System.Taffybar.Widget.Generic.PollingGraph
 import System.Taffybar.Widget.Generic.PollingLabel
 import System.Taffybar.Widget.Util
 import System.Taffybar.Widget.Workspaces
+import qualified Data.List as List
+import qualified Data.List.Split as List
+import qualified Graphics.UI.Gtk as Gtk
 
 main :: IO ()
 main =
@@ -49,7 +31,8 @@ main =
         , textBatteryNew "$percentage$% \62016"
         , pollingGraphNew memCfg 1 memCallback
         , pollingGraphNew cpuCfg 0.5 cpuCallback
-        , networkGraphNew netCfg Nothing
+        , networkMonitorNew defaultNetFormat (Just ["wlp4s0"])
+        , pollingLabelNew "\61931 No Connection" 10 wifiCallback
         ]
       , barPosition = Top
       , barPadding = 0
@@ -60,8 +43,6 @@ main =
     dyreTaffybar $ withBatteryRefresh $ withLogServer $ withToggleServer $ toTaffyConfig myConfig
 
 transparent = (0.0, 0.0, 0.0, 0.0)
-yellow1 = (0.9453125, 0.63671875, 0.2109375, 1.0)
-yellow2 = (0.9921875, 0.796875, 0.32421875, 1.0)
 green1 = (0, 1, 0, 1)
 green2 = (1, 0, 1, 0.5)
 taffyBlue = (0.129, 0.588, 0.953, 1)
@@ -83,11 +64,6 @@ myGraphConfig = defaultGraphConfig
   , graphDirection = RIGHT_TO_LEFT
   }
 
-netCfg = myGraphConfig
-  { graphDataColors = [yellow1, yellow2]
-  , graphLabel = Nothing
-  }
-
 memCfg = myGraphConfig
   { graphDataColors = [taffyBlue]
   , graphLabel = Nothing
@@ -103,52 +79,15 @@ memCallback = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
 
+cpuCallback :: IO [Double]
 cpuCallback = do
   (_, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
---
---wifi :: IO Widget
---wifi =
---  let
---    f = flip fmap (readProcess "nmcli" ["device"] "") $ \s ->
---      case join . join $ conn <$> (words <$> (lines s)) of
---        [] -> colorize "#aaaaaa" "" "\61931 No Connection"
---        x  -> "\61931 " ++ (intercalate " " x)
---
---    conn x = case x of
---      device:typ:"connected":connection -> [connection]
---      _ -> []
---  in
---    pollingLabelNew "\61931 No Connection" 10 f >>= \x ->
---    widgetShowAll x >>
---    pure x
---
----- no idea why the f the name changes...
---network1 :: IO Widget
---network1 = netMonitorNewWith 1 "wlp4s0" 2 "▼ $inKB$kb/s ▲ $outKB$kb/s"
---
---network2 :: IO Widget
---network2 = netMonitorNewWith 1 "wlan0" 2 "▼ $inKB$kb/s ▲ $outKB$kb/s"
 
---battery :: IO Widget
---battery =
---  let f ctxm = fmap (maybe "\62010 --%" id) $ runMaybeT $ do
---        ctx <- MaybeT $ pure ctxm
---        inf <- MaybeT $ getBatteryInfo ctx
---        let icon = case batteryState inf of
---              BatteryStateCharging     -> "\62016"
---              BatteryStateFullyCharged -> "\62016"
---              _ -> if | batteryPercentage inf > 75 -> "\62017"
---                      | batteryPercentage inf > 50 -> "\62018"
---                      | batteryPercentage inf > 25 -> "\62019"
---                      | otherwise -> "\62020"
---        let txt = show (truncate $ batteryPercentage inf) <> "%"
---        pure $ icon <> " " <> txt
---  in
---    batteryContextNew >>= \ctx ->
---    pollingLabelNew "\62010 --%" 10 (f ctx) >>= \x ->
---    widgetShowAll x >>
---    pure x
---
---batteryText :: IO Widget
---batteryText = textBatteryNew "\62016 $percentage$%" 1
+wifiCallback :: IO String
+wifiCallback = do
+  out <- readCreateProcess (shell "nmcli -t dev wifi | grep '*'") ""
+  let ssid = case List.splitOn ":" out of
+        "*":ssid:_-> ssid
+        _ -> "No Connection"
+  pure $ "\61931" <> " " <> ssid

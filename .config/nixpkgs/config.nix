b7cb4b1131ceba02b93012e1b5e73940c90e3ef4
Dennis Kao <dshnkao@gmail.com>
2018-10-21 22:09:44 +1100

    nixpkgs - postman override

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -8,5 +8,14 @@
         sha256 = "0y31g0lxr93370xi74hbpvcy9m81n5wdkdhq8xy2nqp0y4219p13";
       };
     });
+    postman = pkgs.postman.overrideAttrs (oldAttrs: rec {
+      name = "postman-6.4.2";
+      version = "6.4.2";
+      src = pkgs.fetchurl {
+        url = "https://dl.pstmn.io/download/version/${version}/linux64";
+        sha256 = "03p1xc390m16x9rdbsd0a2j1f84rdvl9qjihf5vgqgp17nq9rcha";
+        name = "${name}.tar.gz";
+      };
+    });
   };
 }

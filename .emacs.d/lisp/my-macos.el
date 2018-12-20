9c39d597cb6d755974df2286cd11de15100623a4
Dennis Kao <dennis@cammy.com>
2018-12-21 10:00:17 +1100

    emacs - fix emacs window issues on macOS
    - not focused when switching desktop spaces
    
    - instead of switching to the space emacs currently in,
      emacs windows move to current space when the icon is clicked

--- .emacs.d/lisp/my-macos.el
+++ .emacs.d/lisp/my-macos.el
@@ -1,5 +1,6 @@
 (when (eq system-type 'darwin)
   (setq mac-option-modifier 'meta
-        mac-command-modifier 'super))
+        mac-command-modifier 'super)
+  (menu-bar-mode t))
 
 (provide 'my-macos)
